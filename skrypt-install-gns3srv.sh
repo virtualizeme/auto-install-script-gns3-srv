b4a02e9d375c5baf120a4948bfd740cb7c0b5452
Virtualize Me <118534339+virtualizeme@users.noreply.github.com>
2022-12-10 18:21:05 +0100

    konfiguracje serwera i uzytkownika gns3

--- skrypt-install-gns3srv.sh
+++ skrypt-install-gns3srv.sh
@@ -39,6 +39,84 @@ apt update -y
 sleep 1
 apt install gns3-iou -y
 sleep 1
+
+#################### ta czesc odpowiada za konfiguracje serwera i uzytkownika gns3 #########################
+
+# wpisanie do zmiennej uzytkownika instalujacego serwer
+USER="$(whoami)"
+
+# utworzenie uzytkownika gns3 i przypisanie mu folderu domowego 
+useradd -d /opt/gns3/ -m gns3
+
+# konfiguracja serwera gns3
+mkdir -p /etc/gns3
+cat <<EOFC > /etc/gns3/gns3_server.conf
+[Server]
+host = $(hostname  -I | cut -f1 -d' ')
+port = 3080 
+images_path = /opt/gns3/images
+projects_path = /opt/gns3/projects
+appliances_path = /opt/gns3/appliances
+configs_path = /opt/gns3/configs
+report_errors = True
+[Qemu]
+enable_hardware_acceleration = True
+require_hardware_acceleration = True
+EOFC
+
+chown -R $USER:gns3 /etc/gns3 
+chown -R gns3:gns3 /etc/gns3
+chmod -R 700 /etc/gns3
+
+cat <<EOFI > /etc/init/gns3.conf
+description "GNS3 server"
+author      "GNS3 Team"
+start on filesystem or runlevel [2345]
+stop on runlevel [016]
+respawn
+console log
+script
+    exec start-stop-daemon --start --make-pidfile --pidfile /var/run/gns3.pid --chuid gns3 --exec "/usr/bin/gns3server"
+end script
+pre-start script
+    echo "" > /var/log/upstart/gns3.log
+    echo "[`date`] GNS3 Starting"
+end script
+pre-stop script
+    echo "[`date`] GNS3 Stopping"
+end script
+EOFI
+
+chown root:root /etc/init/gns3.conf
+chmod 644 /etc/init/gns3.conf
+
+# instalacja serwisu systemd
+
+cat <<EOFI > /lib/systemd/system/gns3.service
+[Unit]
+Description=GNS3 server
+After=network-online.target
+Wants=network-online.target
+Conflicts=shutdown.target
+[Service]
+User=gns3
+Group=gns3
+PermissionsStartOnly=true
+EnvironmentFile=/etc/environment
+ExecStartPre=/bin/mkdir -p /var/log/gns3 /var/run/gns3
+ExecStartPre=/bin/chown -R gns3:gns3 /var/log/gns3 /var/run/gns3
+ExecStart=/usr/bin/gns3server --log /var/log/gns3/gns3.log
+ExecReload=/bin/kill -s HUP $MAINPID
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=16384
+[Install]
+WantedBy=multi-user.target
+EOFI
+chmod 755 /lib/systemd/system/gns3.service
+chown root:root /lib/systemd/system/gns3.service
+
+
 # pobranie skryptu do utowrzenia licencji dla IOU, zmiana nazwy pliku i przeniesienie go do konfiguracji gns3
 wget http://www.ipvanquish.com/download/CiscoIOUKeygen3f.py
 python3 ./CiscoIOUKeygen3f.py
@@ -60,15 +138,14 @@ apt install docker-ce -y
 
 
 clear
-echo "Dodanie biezacego uzytkownika do grup  ubridge,libvirt,kvm,wireshark,docker i nadanie uprawnien"
+echo "Dodanie uzytkownika gns3 do grup  ubridge,libvirt,kvm,docker i nadanie uprawnien"
 sleep 2
 
-USER="$(whoami)"
-usermod -aG ubridge $USER
-usermod -aG libvirt $USER
-usermod -aG kvm $USER
+usermod -aG ubridge gns3
+usermod -aG libvirt gns3
+usermod -aG kvm gns3
 # usermod -aG wireshark $USER ==> wpis nie aktywny z uwagi na tryb NOINTERACTIVE instalacji gns3server
-usermod -aG docker $USER
+usermod -aG docker gns3
 sleep 3
 
 clear
